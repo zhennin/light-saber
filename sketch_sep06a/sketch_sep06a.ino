be82781d2632d39286f62d4d6cd35701d285aa09
zhennin <zhennin@aapl000624.local>
2016-09-21 09:54:40 -0400

    adding push button

--- sketch_sep06a/sketch_sep06a.ino
+++ sketch_sep06a/sketch_sep06a.ino
@@ -7,16 +7,23 @@ void setup() {
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(7, INPUT);
+
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ int digitalValue = digitalRead(7);
+ Serial.println(digitalValue);
+ delay(300);
+
+ /* 
  for (int i = 7; i <= 13;  i++) {
   digitalWrite(i, HIGH);
   delay(quickness);
   digitalWrite(i, LOW);
   
   }
+  */
 }
